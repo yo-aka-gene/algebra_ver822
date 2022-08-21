ec23f483939d5d895195852e5a76ab9a479b4429
yo-aka-gene <yujiokano@keio.jp>
2022-08-21 09:46:37 +0900

    :sparkles: add fmt_rpm_sdf

--- tools/preprocess/__init__.py
+++ tools/preprocess/__init__.py
@@ -1,10 +1,11 @@
-from ._normalization import count2rpm, fmt_rpm
+from ._normalization import count2rpm, fmt_rpm, fmt_rpm_sdf
 from ._checker import find
 from ._table import fmt_table
 
 __all__ = [
     "count2rpm",
     "fmt_rpm",
+    "fmt_rpm_sdf",
     "find",
     "fmt_table",
 ]
