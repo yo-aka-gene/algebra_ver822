b5baa2d16305c2b744dc0a745ed9c476eee27126
yo-aka-gene <yujiokano@keio.jp>
2022-08-26 00:20:26 +0900

    :sparkles: add fmt_raw

--- tools/preprocess/__init__.py
+++ tools/preprocess/__init__.py
@@ -1,4 +1,4 @@
-from ._normalization import count2rpm, fmt_rpm, fmt_rpm_sdf
+from ._normalization import count2rpm, fmt_rpm, fmt_rpm_sdf, fmt_raw
 from ._checker import find
 from ._table import fmt_table
 
@@ -8,4 +8,5 @@ __all__ = [
     "fmt_rpm_sdf",
     "find",
     "fmt_table",
+    "fmt_raw",
 ]
