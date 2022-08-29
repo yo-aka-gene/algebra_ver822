987b11efc1cb6ae0fa16f096d35e5d32ee7dc3cc
yo-aka-gene <yujiokano@keio.jp>
2022-08-29 16:18:40 +0900

    :sparkles: add log-normalization

--- tools/spongy_panda/__init__.py
+++ tools/spongy_panda/__init__.py
@@ -1,6 +1,7 @@
 from ._sparse import SparseDF, concat, force_concat
 from ._reader import read_csv, read_pickle, load_npz, load_mtx, load_10xdir
 from ._converter import numpy2sdf, pandas2sdf, list2sdf
+from ._lognormalize import log10normalize, log2normalize, lnnormalize
 
 __all__ = [
     "SparseDF",
@@ -13,5 +14,8 @@ __all__ = [
     "load_10xdir",
     "numpy2sdf",
     "pandas2sdf",
-    "list2sdf"
+    "list2sdf",
+    "log10normalize",
+    "log2normalize",
+    "lnnormalize",
 ]
---
+++ tools/spongy_panda/_lognormalize.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+import numpy as np
+
+from ._sparse import SparseDF
+
+def log2normalize(
+    sdf: SparseDF,
+    adjust_val: Union[int, float] = 1
+) -> SparseDF:
+    data = sdf().tocoo()
+    data.data = np.log2(data.data + adjust_val)
+    return SparseDF(data=data.tocsc(), index=sdf.index, columns=sdf.columns)
+
+
+def log10normalize(
+    sdf: SparseDF,
+    adjust_val: Union[int, float] = 1
+) -> SparseDF:
+    data = sdf().tocoo()
+    data.data = np.log10(data.data + adjust_val)
+    return SparseDF(data=data.tocsc(), index=sdf.index, columns=sdf.columns)
+
+
+def lnnormalize(
+    sdf: SparseDF,
+    adjust_val: Union[int, float] = 1
+) -> SparseDF:
+    data = sdf().tocoo()
+    data.data = np.log(data.data + adjust_val)
+    return SparseDF(data=data.tocsc(), index=sdf.index, columns=sdf.columns)
