5065e97fe0258778c140e88b83ee5d610a96213c
yo-aka-gene <yujiokano@keio.jp>
2022-09-03 08:02:09 +0900

    :sparkles: add cns_subtype_markes

--- tools/figure/_preference.py
+++ tools/figure/_preference.py
@@ -32,6 +32,62 @@ cns_markers = [
     'PECAM1'
 ]
 
+cns_subtype_markers = [
+    # glia markers
+    "SLC1A2", "VIM", "AQP4",
+
+    # astrocyte markers
+    "GFAP", "S100B", "SLC1A3", "ALDH1L1", "BYSL",
+    "GJA1", "GLUL", "PYGB", 
+
+    # oligodendrocyte markers
+    "OLIG2", "CNP", "CA2", "NFIA", "NFIB",
+
+    # OPC mekrers
+    "PDGFRA",
+
+    # Microglial markers
+    "SPP1",
+
+    # neuron markers
+    "RBFOX3", "TBR1", "SOX5", "DCX", "BCL11B",
+    "FEZF2", "SATB2", "CUX1", "CUX2", "POU3F2",
+    "POU3F3", "TUBB3",
+
+    # CGE markers
+    "CALB2", "RELN", "NR2F1", "NR2F2", "VIP",
+
+    # Excitatory Neuron markers
+    "NEUROD1", "NEUROD2", "NEUROD6", "GRIN2B",
+
+    # LGE markers
+    "SIX3", "ISL1", "EBF1",
+
+    # MGE
+    "LHX6", "LHX8", "MAF", "SST", "ERBB4",
+    "SOX6", "NKX2-1",
+
+    # NPC
+    "HES1", "HES5", "TYMS", "FABP7", "EOMES",
+    "NEUROG1", "NCAM1", "TTYH1", "DLX2", "GAD2",
+    "ASCL1", "NEUROG1", "NEUROG2", "PROX1", "TOP2A",
+    "NUSAP1", "NR2E1", "CD24",
+
+    # NSC
+    "PAX6", "NES", "SOX1", "SOX2", "MCM2",
+    "PCNA", "MKI67", "FOXO3", "BHLHE22", "REST",
+
+    # RBC
+    "HBB", "HBM", "HBA1", "HBA2",
+
+    # Endothelial
+    "IGFBP7", "PCAM1",
+
+    # Epithelial
+    "KRT14", "KRT16", "KRT17", "EMX2"
+
+]
+
 def custom_bwr(
     array: np.ndarray,
     centor: Union[int, float],
