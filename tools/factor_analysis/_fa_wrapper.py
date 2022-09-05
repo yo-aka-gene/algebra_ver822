643b47291397d4bc53636d92952493ff001b3fa5
yo-aka-gene <yujiokano@keio.jp>
2022-09-06 08:58:29 +0900

    :sparkles: add AutoFactorAnalyzer

--- tools/factor_analysis/_fa_wrapper.py
+++ tools/factor_analysis/_fa_wrapper.py
@@ -1,10 +1,12 @@
 import factor_analyzer as fa
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
 from tools.figure import kwarg_mgr
+from ._pa import parallel_analysis
 
 class FactorAnalyzer():
     def __init__(
@@ -150,3 +152,120 @@ class FactorAnalyzer():
         )
 
         ax.legend();
+
+
+class AutoFactorAnalyzer():
+    def __init__(
+        self,
+        data: pd.core.frame.DataFrame,
+        rotation: str,
+        random_state: int = 0,
+        nullmodel: str = "norm",
+        use_smc: bool = False,
+        method: str = "minres",
+        threshold: float = 0.5,
+        use_kaiser_criterion: bool = False,
+    ):
+
+        n_f = parallel_analysis(
+            data,
+            random_state=random_state,
+            nullmodel=nullmodel,
+            use_smc=use_smc
+        )
+
+        model = FactorAnalyzer(
+            n_factor=n_f, data=data
+        )
+
+        model.rotate(
+            rotation=rotation,
+            method=method,
+            n_factor=n_f
+        )
+
+        while not np.all(
+            np.abs(model.loading).max(axis=0) > threshold
+        ):
+            if use_kaiser_criterion:
+                n_f = len(
+                    model.model.get_eigenvalues()[1][
+                        model.model.get_eigenvalues()[1] > 1
+                    ]
+                )
+            
+            else:
+                n_f = parallel_analysis(
+                    model.result,
+                    random_state=random_state,
+                    nullmodel=nullmodel,
+                    use_smc=use_smc
+                )
+            
+            model.rotate(
+                rotation=rotation,
+                method=method,
+                n_factor=n_f
+            )
+        
+        self._model_ = model
+        self.model = model.model
+        self.models = model.models
+        self.result = model.result
+        self.results = model.results
+        self.communality = model.communality
+        self.communalities = model.communalities
+        self.uniqueness = model.uniqueness
+        self.uniquenesses = model.uniquenesses
+        self.data = model.data
+        self.n_f = model.n_f
+        self.figs = model.figs
+        
+        return model
+    
+
+    def overwrite(self):
+        self.model = self._model_.model
+        self.models = self._model_.models
+        self.result = self._model_.result
+        self.results = self._model_.results
+        self.communality = self._model_.communality
+        self.communalities = self._model_.communalities
+        self.uniqueness = self._model_.uniqueness
+        self.uniquenesses = self._model_.uniquenesses
+        self.data = self._model_.data
+        self.n_f = self._model_.n_f
+        self.figs = self._model_.figs
+
+    def rotate(
+        self,
+        rotation: str,
+        method: str = "minres",
+        n_factor: int = None
+    ):
+        self._model_.rotate(
+            rotation=rotation,
+            method = method,
+            n_factor=n_factor
+        )
+        
+        self.overwrite()
+
+    def heatmap(self, ax: None, condition: tuple = None, **kwargs):
+        self._model_.heatmap(
+            ax=ax,
+            condition=condition,
+            **kwargs
+        )
+
+        self.overwrite()
+    
+
+    def plot_var(self, ax: None, condition: tuple = None, **kwargs):
+        self._model_.plot_var(
+            ax=ax,
+            condition=condition,
+            **kwargs
+        )
+
+        self.overwrite()
